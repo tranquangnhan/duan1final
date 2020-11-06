60ffcd28cf2076bf58e580e8f255aa7e4cb15b71
tranquangnhan <tranquangnhan1606@gmail.com>
2020-11-06 21:40:27 +0700

    add file

--- admin/views/layouts/header.php
+++ admin/views/layouts/header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <base href="/thuexeasm/admin/">
+    <base href="/duan1final/admin/">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="views/css/style.css">
--- system/config.php
+++ system/config.php
@@ -3,9 +3,10 @@ define('HOST_DB','localhost');
 define('NAME_DB','thuexe');
 define('USER_DB','root');
 define('PASS_DB','');
-define('ROOT_URL','/thuexeasm');
+define('ROOT_URL','/duan1final');
 define('ADMIN_URL',ROOT_URL.'/admin');
 define('SITE_URL',ROOT_URL.'/site/');
 define('SYSTEM_PATH',ROOT_URL.'/system');
 define('PAGE_SIZE',6);
+
 ?>
