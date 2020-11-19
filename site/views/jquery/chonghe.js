183d07d032dd7640adcbaba8bf9e84cd2a1bd03a
tranquangnhan <tranquangnhan1606@gmail.com>
2020-11-19 15:29:44 +0700

    sua lai phan chon ghe

--- site/views/jquery/chonghe.js
+++ site/views/jquery/chonghe.js
@@ -1,45 +1,52 @@
 // window.onload = function() {
+function fireErr(text) {
+    Swal.fire({
+        icon: 'error',
+        type: 'error',
+        title: 'Oops...',
+        text: text,
+        showConfirmButton: true,
+        showCancelButton: false,
+    });
+}
+
 function ClickBtn() {
-    function fireErr(text) {
-        Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: text,
-            showConfirmButton: true,
-            showCancelButton: false,
-        });
-    }
+
     $('.l-ghe-thuonggia').each(function() {
+        $('.l-bg-gray').each(function() {
+            let ThisBox = $(this);
+            ThisBox.click(async function(Events) {
+                Events.preventDefault();
+                fireErr('Ghế không còn trống !');
+            })
+        })
         let ThisBox = $(this);
         ThisBox.click(async function(Events) {
-
-            let Loading = Swal.fire({ // sweetAlert
-                allowEscapeKey: false,
-                title: 'Đang kiểm tra',
-                allowOutsideClick: false,
-                showConfirmButton: false,
-                text: 'Vui lòng chờ trong giây lát...',
-                imageUrl: 'views/img/Default/Loading.gif',
-            });
-
             Events.preventDefault();
-            let ID = ThisBox.text();
-            let idChuyenBay = $("#idcb").val();
+            if ($('.l-ghe-active.l-ghe-thuonggia').length >= 3) {
+                fireErr('Bạn chỉ được đặt tối đa 3 vé !');
+                return;
+            }
 
-            var setGhe = new FormData();
-            setGhe.append('Action', 'setghetg');
-            setGhe.append('idghe', ID);
-            setGhe.append('idcb', idChuyenBay);
-            await $.ajax({
-                type: "POST",
-                url: 'controllers/ajax/chonghe.php',
-                dataType: 'JSON',
-                cache: false,
-                contentType: false,
-                processData: false,
-                data: setGhe,
-                success: function(Data) {}
-            })
+            if (flag === true) {
+                let ID = ThisBox.text();
+                let idChuyenBay = $("#idcb").val();
+
+                var setGhe = new FormData();
+                setGhe.append('Action', 'setghetg');
+                setGhe.append('idghe', ID);
+                setGhe.append('idcb', idChuyenBay);
+                await $.ajax({
+                    type: "POST",
+                    url: 'controllers/ajax/chonghe.php',
+                    dataType: 'JSON',
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    data: setGhe,
+                    success: function(Data) {}
+                })
+            }
         })
     })
 
@@ -52,24 +59,30 @@ function ClickBtn2() {
         ThisBox.click(async function(Events) {
 
             Events.preventDefault();
-            let ID = ThisBox.text();
-            let idChuyenBay = $("#idcb").val();
+            if ($('.l-ghe-active.l-ghe-phothong').length >= 3) {
+                fireErr('Bạn chỉ được đặt tối đa 3 vé !');
+                return;
+            } else {
+                let ID = ThisBox.text();
+                let idChuyenBay = $("#idcb").val();
 
-            var setGhe = new FormData();
-            setGhe.append('Action', 'setghethuong');
-            setGhe.append('idghe', ID);
-            setGhe.append('idcb', idChuyenBay);
+                var setGhe = new FormData();
+                setGhe.append('Action', 'setghethuong');
+                setGhe.append('idghe', ID);
+                setGhe.append('idcb', idChuyenBay);
 
-            await $.ajax({
-                type: "POST",
-                url: 'controllers/ajax/chonghe.php',
-                dataType: 'JSON',
-                cache: false,
-                contentType: false,
-                processData: false,
-                data: setGhe,
-                success: function(Data) {}
-            })
+                await $.ajax({
+                    type: "POST",
+                    url: 'controllers/ajax/chonghe.php',
+                    dataType: 'JSON',
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    data: setGhe,
+                    success: function(Data) {}
+                });
+            }
+
         })
     })
 
